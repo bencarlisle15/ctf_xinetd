f9ef41c173fcb73f57fffb4cacc9d3025988ff03
Ben Carlisle <bencarlisle15@gmail.com>
2021-04-03 20:19:37 -0400

    Init automatic creation

---
+++ entry.py
@@ -0,0 +1,77 @@
+import socket
+import sys
+import docker
+import time
+import select
+from threading import Thread
+
+BUF_SIZE = 1024
+
+HOST = "0.0.0.0"
+REMOTE_PORT = int(sys.argv[1])
+CLIENT_PORT = int(sys.argv[2])
+
+IMAGE = sys.argv[3]
+
+DOCKER_NET = sys.argv[4]
+
+IP_LOOKUP = {}
+
+def create_new_container(client, remote_ip):
+    container = client.containers.run(
+        IMAGE,
+        network=DOCKER_NET,
+        detach=True
+    )
+    container_id = container.attrs["Id"]
+    IP_LOOKUP[remote_ip] = container_id
+    return container_id
+
+def get_container_ip(remote_ip):
+    client = docker.from_env()
+    if remote_ip not in IP_LOOKUP:
+        container_id = create_new_container(client, remote_ip)
+    else:
+        container_id = IP_LOOKUP[remote_ip]
+        try:
+            container = client.containers.get(container_id)
+        except docker.errors.NotFound:
+            container_id = create_new_container(client, remote_ip)
+    print("Container: " + container_id)
+    container = client.containers.get(container_id)
+    ip = container.attrs["NetworkSettings"]["Networks"][DOCKER_NET]["IPAddress"]
+    while ip == "":
+        time.sleep(1)
+        container = client.containers.get(container_id)
+        ip = container.attrs["NetworkSettings"]["Networks"][DOCKER_NET]["IPAddress"]
+    return ip
+
+
+def connection(remote_conn, remote_ip):
+    ip = get_container_ip(remote_ip)
+    with remote_conn:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_conn:
+            print("Connecting to client %s:%d" % (ip, CLIENT_PORT))
+            client_conn.connect((ip, CLIENT_PORT))
+            remote_conn.setblocking(0)
+            client_conn.setblocking(0)
+
+            sockets = [remote_conn, client_conn]
+
+            while True:
+                readable, writable, exceptional = select.select(sockets, sockets, sockets)
+                for readable_sock in readable:
+                    data = readable_sock.recv(BUF_SIZE)
+                    if readable_sock is remote_conn:
+                        client_conn.send(data)
+                    else:
+                        remote_conn.send(data)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, REMOTE_PORT))
+    s.listen()
+    print("Started server: %s:%d" % (HOST, REMOTE_PORT))
+    while True:
+        conn, (remote_ip, remote_port) = s.accept()
+        print("Received connection from %s:%d" % (remote_ip, remote_port))
+        Thread(target=connection, args=[conn, remote_ip]).start()
--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 # Add your startup script
+# systemctl start docker
+# cd /home/ctf/image
+# docker build -t image .
+python3 /entry.py $REMOTE_PORT $CLIENT_PORT $IMAGE $DOCKER_NET
 
 # DO NOT DELETE
 /etc/init.d/xinetd start;
