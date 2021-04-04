6e40985b5ded1880a97bf49f196d586fb49aa3da
Ben Carlisle <bencarlisle15@gmail.com>
2021-04-03 21:19:36 -0400

    Removed comments

--- start.sh
+++ start.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
-# Add your startup script
-# systemctl start docker
-# cd /home/ctf/image
-# docker build -t image .
 python3 /entry.py $REMOTE_PORT $CLIENT_PORT $IMAGE $DOCKER_NET
 
 # DO NOT DELETE
